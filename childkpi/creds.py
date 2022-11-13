5f4c2b797115f955966a7049e9af9d17c5a236da
Volodymyr Mazur <102700104+VovaMazur@users.noreply.github.com>
2022-11-14 01:13:30 +0200

    Delete creds.py

--- childkpi/creds.py
+++
@@ -1,8 +0,0 @@
-# credentials to DB
-creds = {
-    'user': 'postgres',
-    'pass': 'B7g8Fk4U',
-    'host': 'database-1.czzjq785ipj6.eu-west-1.rds.amazonaws.com',
-    'port': 5432,
-    'dbname': 'childkpis'
-}
